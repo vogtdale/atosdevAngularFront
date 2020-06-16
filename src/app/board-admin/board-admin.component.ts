d40a30ccfa1c83024d84eba100d4961b839b90ab
Xenomorph2 <charlesdickens21@gmail.com>
2020-06-16 12:08:10 +0200

    update

--- src/app/board-admin/board-admin.component.ts
+++ src/app/board-admin/board-admin.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UserService } from '../_services/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-board-admin',
   templateUrl: './board-admin.component.html',
   styleUrls: ['./board-admin.component.css']
 })
-export class BoardAdminComponent implements OnInit {
+export class BoardAdminComponent implements OnInit, OnDestroy {
 
+  private subscribtion: Subscription;
   besoins: string;
   constructor(private userService: UserService) { }
 
   ngOnInit() {
-    this.getdetailsClient();
+    this.subscribtion = this.getdetailsClient();
+  }
+
+  ngOnDestroy(): void {
+    this.subscribtion.unsubscribe();
   }
 
 
